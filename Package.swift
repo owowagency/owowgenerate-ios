2f37bd0c028ca99c67b4d151534c8424a2c7554c
Robbert Brandsma <i@robbertbrandsma.nl>
2020-02-05 10:32:48 +0100

    Expose product in Package.swift

--- Package.swift
+++ Package.swift
@@ -8,13 +8,10 @@ let package = Package(
     platforms: [
         .macOS(.v10_15),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+    products: [
+        .executable(name: "owowgenerate-ios", targets: ["owowgenerate-ios"])
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "owowgenerate-ios",
             dependencies: []),
