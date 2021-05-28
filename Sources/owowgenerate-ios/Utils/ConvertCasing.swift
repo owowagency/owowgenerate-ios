afb388e71d5d911720c70d61382d84b5df292a19
Lenever <onuorah.ikechukwuvictor@gmail.com>
2021-05-28 13:42:21 +0100

    add support for strings without -

--- Sources/owowgenerate-ios/Utils/ConvertCasing.swift
+++ Sources/owowgenerate-ios/Utils/ConvertCasing.swift
@@ -1,11 +1,15 @@
 extension StringProtocol {
     func camelCase(delimiter: Character, upper: Bool) -> String {
-        return self.split(separator: delimiter).enumerated().map { element -> String in
-            if element.offset == 0 && !upper {
-                return String(element.element)
-            }
-            
-            return element.element.capitalized
-        }.reduce("", +)
+        if element.contains(delimiter) {
+            return self.split(separator: delimiter).enumerated().map { element -> String in
+                if element.offset == 0 && !upper {
+                    return element.element.lowercased()
+                }
+                
+                return element.element.capitalized
+            }.reduce("", +)
+        } else {
+            return self
+        }
     }
 }
