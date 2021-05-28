bee76edb1e1f26099993e1fc9b2ec1150c80bcae
Lenever <onuorah.ikechukwuvictor@gmail.com>
2021-05-28 14:23:23 +0100

    return self if missing delimiter

--- Sources/owowgenerate-ios/Utils/ConvertCasing.swift
+++ Sources/owowgenerate-ios/Utils/ConvertCasing.swift
@@ -1,6 +1,6 @@
 extension StringProtocol {
     func camelCase(delimiter: Character, upper: Bool) -> String {
-        if element.contains(delimiter) {
+        if self.contains(delimiter) {
             return self.split(separator: delimiter).enumerated().map { element -> String in
                 if element.offset == 0 && !upper {
                     return element.element.lowercased()
