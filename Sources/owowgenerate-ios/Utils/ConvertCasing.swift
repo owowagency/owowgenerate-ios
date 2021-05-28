293ac495b891ecb8f9767226bbf40476230546a4
Lenever <onuorah.ikechukwuvictor@gmail.com>
2021-05-28 10:47:26 +0100

    return string of element

--- Sources/owowgenerate-ios/Utils/ConvertCasing.swift
+++ Sources/owowgenerate-ios/Utils/ConvertCasing.swift
@@ -2,7 +2,7 @@ extension StringProtocol {
     func camelCase(delimiter: Character, upper: Bool) -> String {
         return self.split(separator: delimiter).enumerated().map { element -> String in
             if element.offset == 0 && !upper {
-                return element.element
+                return String(element.element)
             }
             
             return element.element.capitalized
