e73fc676839e29240985d565428384382a5d395c
Konstantin Velkov <konstantinmvelkov@gmail.com>
2021-09-27 19:35:38 +0300

    Updated access modifiers

--- Sources/owowgenerate-ios/Writers/SwiftUICodeWriter.swift
+++ Sources/owowgenerate-ios/Writers/SwiftUICodeWriter.swift
@@ -17,7 +17,7 @@ fileprivate func writeStrings(strings: StringsCollection, writer: inout SwiftCod
         let variableName = name.camelCase(from: config.caseStyle, upper: false).swiftIdentifier
         let typeName = (name.camelCase(from: config.caseStyle, upper: true) + "StringsNamespace").swiftIdentifier
         
-        writer.addLine("static var \(variableName): \(typeName).Type { \(typeName).self }")
+        writer.addLine("public static var \(variableName): \(typeName).Type { \(typeName).self }")
         
         writer.inBlock("public struct \(typeName)") { writer in
             writeStrings(strings: collection, writer: &writer)
