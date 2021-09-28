3810c716d0eebf2e8516e8c37a410134de88d206
Konstantin Velkov <konstantinmvelkov@gmail.com>
2021-09-28 13:38:31 +0300

    Update NSLocalizedStringWriter.swift

--- Sources/owowgenerate-ios/Writers/NSLocalizedStringWriter.swift
+++ Sources/owowgenerate-ios/Writers/NSLocalizedStringWriter.swift
@@ -11,7 +11,7 @@ func makeLocalizedStringCode(strings: StringsCollection, isForLibrary: Bool) -> 
     
     writer.addLine("import Foundation")
     
-    var extensionText = isConstructingForLibrary ? "public extension SwiftUI.Text" : "extension SwiftUI.Text"
+    var extensionText = isConstructingForLibrary ? "public enum Strings" : "enum Strings"
     
     writer.inBlock(extensionText) { writer in
         writeStrings(strings: strings, writer: &writer)
